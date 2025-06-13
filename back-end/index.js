be3c6e169df5b62cb3b067363b87e32272eddb91
mockqv <richardviniciusfs07@gmail.com>
2025-06-12 23:45:23 -0300

    :recycle: refac: change shelters get by AI agent instead of Mock Data on Supabase

--- back-end/index.js
+++ back-end/index.js
@@ -1,26 +1,63 @@
 import express from 'express';
 import pool from './db.js';
+import dotenv from "dotenv"
+import { googleAI } from '@genkit-ai/googleai';
+import { genkit } from 'genkit';
+dotenv.config()
 const app = express();
 
 app.use(express.json());
 
+const ai = genkit({
+  plugins: [googleAI()],
+  model: googleAI.model('gemini-2.0-flash'), // set default model
+});
+
+const abrigoTemplate = {
+  nome: "Abrigo Exemplo",
+  endereco: "Rua das Flores, 123",
+  tipo: "Temporário",
+  capacidade: 100,
+  recursos: "Água, alimentos, primeiros socorros",
+  latitude: -30.1234,
+  longitude: -51.2345
+};
+
 //Liberando acesso a todos os domínios para testar localmente
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  });
-  
-app.get('/abrigos', async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+// app.get('/abrigos', async (req, res) => {
+//   try {
+//     const result = await pool.query('SELECT * FROM abrigos ORDER BY nome');
+//     res.json(result.rows);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ erro: 'Erro ao buscar abrigos' });
+//   }
+// });
+
+app.get('/getAbrigos', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM abrigos ORDER BY nome');
-    res.json(result.rows);
+    const { text } = await ai.generate(`Por favor envie os abrigos localizados no brasil para abrigar pessoas quando há eventos climáticos de alta intensidade, apenas em formato de array de objeto como esse ${abrigoTemplate}! APENAS MANDE O ARRAY DE OBJETOS NADA MAIS, TODOS OS CAMPOS SÃO DE EXTREMA IMPORTANCIA, NÃO SE ESQUEÇA DE NENHUM`);
+
+    const cleanedResponse = text
+    .replace(/^```json\n/, '')
+    .replace(/\n```$/, '')
+
+    const response = JSON.parse(cleanedResponse)
+    res.status(200).send(response)
+    // return(text)
+
   } catch (err) {
     console.error(err);
     res.status(500).json({ erro: 'Erro ao buscar abrigos' });
   }
-});
+})
 
 app.post('/abrigos', async (req, res) => {
   const { nome, endereco, tipo, capacidade, recursos, latitude, longitude } = req.body;
@@ -46,7 +83,7 @@ app.get('/', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
 });
